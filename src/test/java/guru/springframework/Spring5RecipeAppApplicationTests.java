981390d4490bb360a0491ea6b453a8e1451bcaf0
mamaligamar <maryna.mamaliga@gmail.com>
2020-04-07 19:20:15 +0200

    Tests

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -5,8 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//Is an integration test
+@RunWith(SpringRunner.class) //Run test with spring context
+@SpringBootTest //Search Spring Boot Application Configuration
 public class Spring5RecipeAppApplicationTests {
 
 	@Test
---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,35 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)//The spring context is uprise only one time for all the tests
+@DataJpaTest //Create an embedde database
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired //As we have the spring context up we can inject our dependencies
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    @DirtiesContext //It indicates that we have to uprise a new spring context for this test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
