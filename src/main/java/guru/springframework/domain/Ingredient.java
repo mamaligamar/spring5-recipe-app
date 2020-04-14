bbc74fc04cc98f0226345ecd1ac48e59a90ecced
mamaligamar <maryna.mamaliga@gmail.com>
2020-04-14 19:35:31 +0200

    Adding new ingredient

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -2,11 +2,18 @@ package guru.springframework.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-@Data
-@EqualsAndHashCode(exclude = "recipe")
+
+/**
+ * Created by jt on 6/13/17.
+ */
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
     @Id
@@ -29,4 +36,12 @@ public class Ingredient {
         this.amount = amount;
         this.uom = uom;
     }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
 }
